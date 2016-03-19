7aa6a417a1e2edf1afec6ca95aca37bef5d04b6d
Ted Li <lwxted@Teds-MBP.home>
2016-03-18 23:21:31 -0400

    Temporarily removed shorthand notation for StdEnvs until we later figure out how to correctly handle it.

--- Block/Parser/StdEnv.py
+++ Block/Parser/StdEnv.py
@@ -5,15 +5,9 @@ from n0ted0wn.Block.Parser.Base import Base
 
 class StdEnv(Base):
   """StdEnv parases the block of a standardized format as follows:
-
   {block_type:param1:param2}
   Content lines...
   {block_type}
-
-  Or when the block itself is short and not supposed to contain any
-  content, the ending block marker is not required:
-
-  {block_type:param1:param2}
   """
 
   _block_type = ''
@@ -31,20 +25,17 @@ class StdEnv(Base):
   def parse(cls, raw, style_cls):
     raw = raw.strip()
     first_line_break = raw.find('\n')
-    first_line = raw
-    if first_line_break != -1:
-      first_line = raw[0:first_line_break]
+    if first_line_break == -1:
+      return None
+    first_line = raw[0:first_line_break]
     if first_line[0] != '{' or first_line[-1] != '}':
       return None
     args = first_line[1:-1].split(':')
     if args[0] != cls._block_type:
       return None
-    last_line_break = raw.rfind('\n')
-    if first_line_break == -1 and last_line_break == -1:
-      obj = cls(raw, args[1:], '', style_cls)
-      return obj._transform_args()
     if not raw.endswith('\n{' + cls._block_type + '}'):
       return Base.NOT_COMPLETE
+    last_line_break = raw.rfind('\n')
     obj = cls(
       raw, args[1:], raw[first_line_break + 1:last_line_break], style_cls)
     return obj._transform_args()
