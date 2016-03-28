92c5ed1c977fcbcc5b746e8bc8f29ce7b258024d
Ted Li <lwxted@Teds-MBP.home>
2016-03-28 04:10:08 -0400

    Fixed typo

--- Block/Parser/Separator.py
+++ Block/Parser/Separator.py
@@ -5,7 +5,7 @@ from n0ted0wn.Block.Parser.Base import Base
 
 class Separator(Base):
   def __init__(self, raw, style_cls):
-    super(Separator, self).__init__()
+    super(Separator, self).__init__(raw, style_cls)
 
   @classmethod
   def parse(cls, raw, style_cls):
