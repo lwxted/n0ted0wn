258f50886dc4ba9eefa0f634e165d6bb28dd63cc
Ted Li <tedwxli@gmail.com>
2016-04-13 09:59:58 -0400

    Removed extranous renderer file

--- RendererDel.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from n0ted0wn.Inline.Renderer.Base import Base
-
-class RendererDel(Base):
-  @classmethod
-  def _render(cls, inline, final_process):
-    return '<del>{0}</del>'.format(final_process(inline.content))
