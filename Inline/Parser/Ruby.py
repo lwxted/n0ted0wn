b596d0ba0ddb85a6b604487c0cb16bc807f489ee
Ted Li <tedwxli@gmail.com>
2016-04-28 02:11:39 -0400

    Updated ruby for convenience

--- Inline/Parser/Ruby.py
+++ Inline/Parser/Ruby.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import operator
 import re
 
 from n0ted0wn.Inline.Parser.Base import Base
@@ -8,6 +9,13 @@ from n0ted0wn.Inline.Parser.Base import Base
 class Ruby(Base):
   @classmethod
   def parse(cls, raw):
-    return [cls(\
+    l1 = [cls(\
       raw, m.span(), (raw[slice(*m.span(1))], raw[slice(*m.span(2))])) \
       for m in re.finditer(r'\[(\w+?)\]<(\w+?)>', raw, re.UNICODE)]
+    l2 = [cls(\
+      raw, m.span(), (raw[slice(*m.span(1))], raw[slice(*m.span(2))])) \
+      for m in re.finditer(ur'【(\w+?)】《(\w+?)》', raw, re.UNICODE)]
+    l3 = [cls(\
+      raw, m.span(), (raw[slice(*m.span(1))], raw[slice(*m.span(2))])) \
+      for m in re.finditer(ur'「(\w+?)」＜(\w+?)＞', raw, re.UNICODE)]
+    return sorted(l1 + l2 + l3, key=operator.attrgetter('span'))
